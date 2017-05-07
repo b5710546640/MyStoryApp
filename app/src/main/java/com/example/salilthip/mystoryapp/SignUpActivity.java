7a719ae74f8c8b9cf1138caeaeaa388d963bc06d
unknown <salilthip.p@ku.th>
2017-05-07 18:01:16 +0700

    Application should be signup but it haven't been tested

--- app/src/main/java/com/example/salilthip/mystoryapp/SignUpActivity.java
+++ app/src/main/java/com/example/salilthip/mystoryapp/SignUpActivity.java
@@ -1,6 +1,13 @@
 package com.example.salilthip.mystoryapp;
 
+import android.Manifest;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
 import android.support.annotation.NonNull;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -10,12 +17,21 @@ import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.firebase.client.Firebase;
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.UserProfileChangeRequest;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
 public class SignUpActivity extends AppCompatActivity {
 
@@ -24,14 +40,32 @@ public class SignUpActivity extends AppCompatActivity {
     private ImageButton selectedImage;
     private Button signupBtn;
 
+    public static final int READ_EXTERNAL_STORAGE = 0;
+    private static final int GALLERY_INTENT = 2;
+    private ProgressDialog mProgressDialog;
+    private Firebase mRootRef;
+    private Uri mImageUri = null;
+    private DatabaseReference mDatabaseRef;
+    private StorageReference mStorage;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
 
+        mAuth = FirebaseAuth.getInstance();
+        Firebase.setAndroidContext(this);
+
         emailSignup = (EditText)findViewById(R.id.emailSignup);
         passwordSignup = (EditText)findViewById(R.id.passwordSignup);
         signupBtn = (Button)findViewById(R.id.signupBtn);
+        selectedImage = (ImageButton)findViewById(R.id.selectImageBtn);
+
+        mProgressDialog = new ProgressDialog(SignUpActivity.this);
+
+        mDatabaseRef = FirebaseDatabase.getInstance().getReference();
+        mRootRef = new Firebase("https://mystoryapp-2e9ec.firebaseio.com/").child("User_Details").push();
+        mStorage = FirebaseStorage.getInstance().getReferenceFromUrl("gs://mystoryapp-2e9ec.appspot.com/");
 
         signupBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -41,12 +75,38 @@ public class SignUpActivity extends AppCompatActivity {
                 if(getEmail.isEmpty()||getPassword.isEmpty())
                     Toast.makeText(SignUpActivity.this, "Sign up Failed, please check your email and password",
                             Toast.LENGTH_SHORT).show();
-                else
+                else {
                     callSignup(getEmail, getPassword);
-                finish();
+                    askImageForSignUp();
+                    final String mName = emailSignup.getText().toString().trim();
+                    if (mName.isEmpty()) {
+                        Toast.makeText(getApplicationContext(), "Please Enter Title", Toast.LENGTH_SHORT).show();
+                        return;
+                    }
+                    Firebase childRef_Name = mRootRef.child("profile_owner");
+                    childRef_Name.setValue(mName);
+                    Toast.makeText(getApplicationContext(), "Update Info", Toast.LENGTH_SHORT).show();
+                }
             }
         });
 
+    }
+
+    private void askImageForSignUp(){
+        if (ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            Toast.makeText(getApplicationContext(), "Call for permission", Toast.LENGTH_SHORT).show();
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                requestPermissions(new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, READ_EXTERNAL_STORAGE);
+            }
+        } else {
+            callGallery();
+        }
+    }
+
+    private void callGallery() {
+        Intent intent = new Intent(Intent.ACTION_PICK);
+        intent.setType("image/*");
+        startActivityForResult(intent, GALLERY_INTENT);
     }
 
     private void callSignup(String email, String password) {
@@ -72,6 +132,47 @@ public class SignUpActivity extends AppCompatActivity {
                         // ...
                     }
                 });
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case READ_EXTERNAL_STORAGE:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED)
+                    callGallery();
+                return;
+        }
+        Toast.makeText(getApplicationContext(), "....", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode==GALLERY_INTENT && resultCode==RESULT_OK){
+            mImageUri = data.getData();
+            selectedImage.setImageURI(mImageUri);
+            StorageReference filepath = mStorage.child("User_Images").child(mImageUri.getLastPathSegment());
+
+            mProgressDialog.setMessage("Uploading...");
+            mProgressDialog.show();
+
+            filepath.putFile(mImageUri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                    Uri downloadUri = taskSnapshot.getDownloadUrl();
+                    mRootRef.child("Image_URL").setValue(downloadUri.toString());
+                    Glide.with(getApplicationContext())
+                            .load(downloadUri)
+                            .crossFade()
+                            .placeholder(R.drawable.loading)
+                            .diskCacheStrategy(DiskCacheStrategy.RESULT)
+                            .into(selectedImage);
+                    Toast.makeText(getApplicationContext(),"Updated...",Toast.LENGTH_SHORT).show();
+                    mProgressDialog.dismiss();
+                }
+            });
+        }
     }
 
     private void userProfile(){
